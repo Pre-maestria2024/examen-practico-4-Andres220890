22d867ba23773f82ceaa661606140a8e874dfc4e
Andres220890 <candresalberto@gmail.com>
2025-03-08 10:11:15 -0500

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -3,10 +3,40 @@ from queue import Queue
 
 def main():
 
-    n, k = map(int, input().split())
+    ans = 0
     
-    for i in range(n-1):
-        u,v = map(int,input().split())
+    def dfs(current, parent, k, adj):
+        h = 1
+    
+        for to in adj[current]:
+            if to == parent:
+                continue
+    
+            h = max(h, dfs(to, current, k, adj) + 1)
+    
+        # Cada vez que encontramos una altura >= k
+        # Añadimos 1 a la respuesta
+        if h == k:
+            global ans
+            ans += 1
+    
+            # Reiniciamos la altura a 0
+            h = 0
+    
+        return h
+    
+    n, k = map(int, input().split())
+    adj = [[] for _ in range(n)]
+    
+    for _ in range(n-1):
+        u, v = map(int, input().split())
+    
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    dfs(0, -1, k, adj)
+    
+    print(ans)
 
 if __name__  == '__main__':
     main()
